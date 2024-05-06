fd49edac67084cb7beaaf5226bd07b4123840b2f
Sharkhann <137162457+Sharkhann@users.noreply.github.com>
2024-05-06 16:58:28 +0200

    Refactorisation(train_test_split) on train.py

--- src/model/train.py
+++ src/model/train.py
@@ -3,16 +3,21 @@
 import argparse
 import glob
 import os
-
+import numpy as np
 import pandas as pd
-
+import sklearn
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import logging
+import sys
+from autologging import logged, TRACE, traced
 
 
-# define functions
+# define functions.
+@traced
+@logged
 def main(args):
     # TO DO: enable autologging
-
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -22,8 +27,11 @@ def main(args):
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    
+    main._log.info("This is an info for main_message")
 
-
+@traced
+@logged
 def get_csvs_df(path):
     if not os.path.exists(path):
         raise RuntimeError(f"Cannot use non-existent path provided: {path}")
@@ -31,16 +39,29 @@ def get_csvs_df(path):
     if not csv_files:
         raise RuntimeError(f"No CSV files found in provided data path: {path}")
     return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+    
+    get_csvs_df._log.info("This is an info for get_csvc_df_message")
 
 
 # TO DO: add function to split data
+@traced
+@logged
+def split_data(df):
+    X = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values
+    y = df['Diabetic'].values
 
+    split_data._log.info("This is an info for split_data_message")
 
+@traced
+@logged
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
     LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+    
+    train_model._log.info("This is an info for train_model_message")
 
-
+@traced
+@logged
 def parse_args():
     # setup arg parser
     parser = argparse.ArgumentParser()
